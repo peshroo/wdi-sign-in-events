46db6874abf70488c0b91dab2629effb54aa3bc4
Roopesh <rrattan.89@gmail.com>
2017-07-18 14:51:11 -0400

    submit button, added error class for input

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function(event){
   var signIn = document.querySelector('.signin');
   var modal  = document.querySelector('.modal');
   var close  = document.querySelector('.close');
+  var submit = document.querySelector('.submit');
+  var input  = document.querySelector('input');
 
   signIn.addEventListener('click', function(){
     modal.style.display = 'block';
@@ -9,6 +11,12 @@ document.addEventListener('DOMContentLoaded', function(event){
 
   close.addEventListener('click', function(){
     modal.style.display = 'none';
+  });
+
+  submit.addEventListener('click', function(){
+    for(var i = 0; i < input.length; i++) {
+      input[i].className = 'error';
+    }
   });
 
 });
